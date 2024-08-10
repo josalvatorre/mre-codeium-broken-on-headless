1eaeb337e8c1af80befebb517b7b4beda9f4b554
Jose Alvarado <jos.alvatorre@gmail.com>
2024-08-10 23:44:42 +0000

    event=BufEnter

--- init.lua
+++ init.lua
@@ -28,7 +28,13 @@ require("lazy").setup({
   spec = {
     -- add your plugins here
     {
+        -- I took this from the .nvim installation instructions.
+        -- https://github.com/Exafunction/codeium.nvim/blob/f6a2ef32a9e923cb0104a19d3e426b0e40e49505/README.md?plain=1#L49-L61
         "Exafunction/codeium.nvim",
+        -- I took this from the .vim installation instructions. Codeium doesn't work without this line for me.
+        -- I don't understand why there is both a .vim and .nvim version of the same plugin.
+        -- https://github.com/Exafunction/codeium.vim/blob/0b74f274b82c00eaad4b5b5cd8b2e76f872d19f1/README.md?plain=1#L206
+        event = 'BufEnter',
         dependencies = {
             "nvim-lua/plenary.nvim",
             "hrsh7th/nvim-cmp",
